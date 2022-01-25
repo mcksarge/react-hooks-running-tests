a388151d719d00eff78174952a2746e6b6916f8d
Fred <higgins.fred.s@gmail.com>
2022-01-24 16:01:38 -0800

    commit tree 251b62a6440b8f6048b9bbe250aff7906a4241bc

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>please pass this test</div>
 }
+
+screen.debug();
 
 export default Article;
